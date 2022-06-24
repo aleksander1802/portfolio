94b6b405cdd839c4d998451b311c1f2c93188587
Alex2 <aleksander1802@gmail.com>
2022-06-24 17:42:03 +0300

    init: adaptive1

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,7 +3,8 @@ const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       close = document.querySelector('.menu__close'),
       counter = document.querySelectorAll('.skills__ratings-counter'),
-      lines = document.querySelectorAll('.skills__ratings-line span');
+      lines = document.querySelectorAll('.skills__ratings-line span'),
+      overlay = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -17,6 +18,20 @@ counter.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
 
+
+overlay.addEventListener('click', (e) => {
+    if (e.target) {
+        menu.classList.remove('active');
+    }
+});
+
+function searchRemove(e) {
+    if (e.code === 'Escape' && menu.classList.contains('active')) {
+        menu.classList.remove('active');
+    }
+}
+
+document.addEventListener('keydown', searchRemove);
 
 
 
